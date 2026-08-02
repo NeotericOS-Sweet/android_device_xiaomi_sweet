a8b6fe8b0e504b932f6be604d6327e9eeb9ce612
imranpopz <imranpopz32@gmail.com>
2026-08-02 18:41:11 +0200

    sweet: Weicome Neoteric OS

--- sweet.mk
+++ sweet.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from the Neoteric configuration.
+$(call inherit-product, vendor/neoteric/target/product/neoteric-target.mk)
 
-PRODUCT_NAME := lineage_sweet
+PRODUCT_NAME := sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
